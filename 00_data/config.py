8c8e05cbcfd94d8e36f19926170398e594e4f84f
Lane Miller <millerlanep@gmail.com>
2026-04-29 16:53:56 -0400

    Add config.py to store important constants; replace constant definitions in 00_build_dataset with config import

---
+++ 00_data/config.py
@@ -0,0 +1,17 @@
+# 00_data/config.py
+from pathlib import Path
+
+FS = 16000
+BS = 1024
+ALPHA_MIN = 0.05
+ALPHA_MAX = 0.95
+SILENCE_THRESH = 0.025
+N_TEST_TLKR = 20
+N_TEST_UTT_PER_TLKR = 5
+TRAIN_HR = 4.0
+VAL_HR = 0.5
+
+LIBRISPEECH_ROOT = Path("/Volumes/LPM03 storage/Datasets/Audio/LibriSpeech")
+TRAIN_OUT = Path("/Volumes/LPM03 storage/Datasets/Audio/ProjectTrainValTest/speech-declipping/train")
+VAL_OUT = Path("/Volumes/LPM03 storage/Datasets/Audio/ProjectTrainValTest/speech-declipping/val")
+TEST_OUT = Path("/Volumes/LPM03 storage/Datasets/Audio/ProjectTrainValTest/speech-declipping/test")
